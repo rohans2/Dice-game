54f03a3cbbd28e7cc6505a27a2a14fa4146c390b
ap14087 <65537606+ap14087@users.noreply.github.com>
2020-05-26 05:44:17 -0400

    add jquery and ejs6

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var randomno1=6*Math.random()+1;
+const randomno1=6*Math.random()+1;
 randomno1=Math.floor(randomno1);
-var images="images/dice"+randomno1+".png";
-document.querySelectorAll("img")[0].setAttribute("src",images);
+const images="images/dice"+randomno1+".png";
+$("img")[0].setAttribute("src",images);
 
-var randomno2=6*Math.random()+1;
+let randomno2=6*Math.random()+1;
 randomno2=Math.floor(randomno2);
-var images2="images/dice"+randomno2+".png";
-document.querySelectorAll("img")[1].setAttribute("src",images2);
+let images2="images/dice"+randomno2+".png";
+$("img")[1].setAttribute("src",images2);
 
 if(randomno1>randomno2)
 {
-  document.querySelector("h1").innerHTML="🚩 Player 1 Wins!";
+  $("h1").innerHTML="🚩 Player 1 Wins!";
 }
 else if(randomno2>randomno1)
 {
-  document.querySelector("h1").innerHTML=" Player 2 Wins 🚩";
+  $("h1").innerHTML=" Player 2 Wins 🚩";
 }
 else{
-  document.querySelector("h1").innerHTML="DRAW!!";
+  $("h1").innerHTML="DRAW!!";
 }
